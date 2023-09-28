f1ad3efbb7f980d6fc6c8369f5b8eaad10963fe6
SP4CEVI <95271930+SP4CEVI@users.noreply.github.com>
2023-09-28 17:49:06 +0300

    Add files via upload

---
+++ 9a.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void find_and_swap_min_max(int arr[], int size) {
+    int minIndex = 0;
+    int maxIndex = 0;
+    
+    for (int i = 1; i < size; i++) {
+        if (arr[i] < arr[minIndex]) {
+            minIndex = i;
+        }
+        if (arr[i] > arr[maxIndex]) {
+            maxIndex = i;
+        }
+    }
+    
+    swap(&arr[minIndex], &arr[maxIndex]);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Incorrect number of arguments.\n");
+        return 1;
+    }
+    
+    int a = atoi(argv[1]);
+    int b = atoi(argv[2]);
+    
+    srand(time(NULL));
+    
+    int size = 10;
+    int arr[size];
+    printf("Massive before changes: ");
+    for (int i = 0; i < size; i++) {
+        arr[i] = a + rand() % b;
+        printf("%d ", arr[i]);
+    }
+    
+    printf("\n");
+    
+    find_and_swap_min_max(arr, size);
+    printf("Massive after changes: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    
+    printf("\n");
+    
+    return 0;
+}
