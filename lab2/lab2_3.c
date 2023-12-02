72996da952488aad5907d43139b029dbd2d1ee00
SP4CEVI <95271930+SP4CEVI@users.noreply.github.com>
2023-12-02 15:05:37 +0300

    Update lab2_3.c

--- lab2/lab2_3.c
+++ lab2/lab2_3.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include <string.h>
 #include <stddef.h>
 
 enum Status {
@@ -10,23 +9,47 @@ enum Status {
     SUBSTRING_NOT_FOUND
 };
 
-enum Status find_substring_in_files(const char *substring, int count, ...) {
+char* my_strstr(const char* haystack, const char* needle) {
+    if (*needle == '\0') {
+        return (char*)haystack;
+    }
+    
+    while (*haystack) {
+        const char* h = haystack;
+        const char* n = needle;
+        
+        while (*h && *n && (*h == *n)) {
+            h++;
+            n++;
+        }
+        
+        if (*n == '\0') {
+            return (char*)haystack;
+        }
+        
+        haystack++;
+    }
+    
+    return NULL;
+}
+
+enum Status find_substring_in_files(const char* substring, int count, ...) {
     va_list files;
     va_start(files, count);
 
     enum Status status = SUCCESS;
 
     for (int i = 0; i < count; i++) {
-        const char *filename = va_arg(files, const char *);
+        const char* filename = va_arg(files, const char*);
 
-        FILE *file = fopen(filename, "r");
+        FILE* file = fopen(filename, "r");
         if (!file) {
             printf("Failed to open file: %s\n", filename);
             status = ERROR_FILE_OPEN;
             continue;
         }
 
-        char *line = NULL;
+        char* line = NULL;
         size_t line_size = 0;
         ssize_t read;
 
@@ -34,9 +57,9 @@ enum Status find_substring_in_files(const char *substring, int count, ...) {
         int found = 0;
 
         while ((read = getline(&line, &line_size, file)) != -1) {
-            char *position = line;
+            char* position = line;
 
-            while ((position = strstr(position, substring)) != NULL) {
+            while ((position = my_strstr(position, substring)) != NULL) {
                 int column_number = position - line + 1;
 
                 printf("File: %s, Line: %d, Char: %d\n", filename, line_number, column_number);
@@ -62,7 +85,7 @@ enum Status find_substring_in_files(const char *substring, int count, ...) {
 }
 
 int main() {
-    enum Status result = find_substring_in_files("111", 3, "file1.txt", "file2.txt", "file3.txt");
+    enum Status result = find_substring_in_files("h", 3, "file1.txt", "file2.txt", "file3.txt");
     printf("Search completed successfully\n");
     return 0;
 }
